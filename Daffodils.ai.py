a31978f4d681464380d4bdb82d42449f347d7fbb
nd1462 <35877645+nd1462@users.noreply.github.com>
2019-05-20 08:27:14 -0400

    Add files via upload

---
+++ Daffodils.ai.py
@@ -0,0 +1,686 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# Poetry Generator
+
+# Using First LSTM model to generate some paragraph format of text
+
+# In[ ]:
+
+
+import string
+# load doc into memory
+def load_doc(filename):
+	# open the file as read only
+	file = open(filename, 'r')
+	# read all text
+	text = file.read()
+	# close the file
+	file.close()
+	return text
+ 
+# turn a doc into clean tokens
+def clean_doc(doc):
+	# replace '--' with a space ' '
+	doc = doc.replace('--', ' ')
+	# split into tokens by white space
+	tokens = doc.split()
+	# remove punctuation from each token
+	table = str.maketrans('', '', string.punctuation)
+	tokens = [w.translate(table) for w in tokens]
+	# remove remaining tokens that are not alphabetic
+	tokens = [word for word in tokens if word.isalpha()]
+	# make lower case
+	tokens = [word.lower() for word in tokens]
+	return tokens
+ 
+# save tokens to file, one dialog per line
+def save_doc(lines, filename):
+	data = '\n'.join(lines)
+	file = open(filename, 'w')
+	file.write(data)
+	file.close()
+ 
+# load document
+
+in_filename = 'Data/notorious-big.txt'
+doc = load_doc(in_filename)
+print(doc[:200])
+ 
+# clean document
+tokens = clean_doc(doc)
+print(tokens[:200])
+print('Total Tokens: %d' % len(tokens))
+print('Unique Tokens: %d' % len(set(tokens)))
+ 
+# organize into sequences of tokens
+length = 50 + 1
+sequences = list()
+for i in range(length, len(tokens)):
+	# select sequence of tokens
+	seq = tokens[i-length:i]
+	# convert into a line
+	line = ' '.join(seq)
+	# store
+	sequences.append(line)
+print('Total Sequences: %d' % len(sequences))
+ 
+# save sequences to file
+out_filename = 'republic_sequences.txt'
+save_doc(sequences, out_filename)
+
+
+# In[ ]:
+
+
+import numpy as np
+from matplotlib import pyplot as plt
+get_ipython().run_line_magic('matplotlib', 'inline')
+def plotWordFrequency(input):
+    f = open(artist_file,'r')
+    words = [x for y in [l.split() for l in f.readlines()] for x in y]
+    data = sorted([(w, words.count(w)) for w in set(words)], key = lambda x:x[1], reverse=True)[:40] 
+    most_words = [x[0] for x in data]
+    times_used = [int(x[1]) for x in data]
+    plt.figure(figsize=(20,10))
+    plt.bar(x=sorted(most_words), height=times_used, color = 'grey', edgecolor = 'black',  width=.5)
+    plt.xticks(rotation=45, fontsize=18)
+    plt.yticks(rotation=0, fontsize=18)
+    plt.xlabel('Most Common Words:', fontsize=18)
+    plt.ylabel('Number of Occurences:', fontsize=18)
+    plt.title('Most Commonly Used Words: %s' % (artist_file), fontsize=24)
+    plt.show()
+
+
+# In[ ]:
+
+
+artist_file = 'Data/notorious-big.txt'
+plotWordFrequency(artist_file)
+
+
+# In[ ]:
+
+
+from numpy import array
+from pickle import dump
+from keras.preprocessing.text import Tokenizer
+from keras.utils import to_categorical
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.layers import LSTM
+from keras.layers import Embedding
+ 
+# load doc into memory
+def load_doc(filename):
+	# open the file as read only
+	file = open(filename, 'r')
+	# read all text
+	text = file.read()
+	# close the file
+	file.close()
+	return text
+ 
+# load
+in_filename = 'republic_sequences.txt'
+doc = load_doc(in_filename)
+lines = doc.split('\n')
+ 
+# integer encode sequences of words
+tokenizer = Tokenizer()
+tokenizer.fit_on_texts(lines)
+sequences = tokenizer.texts_to_sequences(lines)
+# vocabulary size
+vocab_size = len(tokenizer.word_index) + 1
+ 
+# separate into input and output
+sequences = array(sequences)
+X, y = sequences[:,:-1], sequences[:,-1]
+y = to_categorical(y, num_classes=vocab_size)
+seq_length = X.shape[1]
+ 
+# define model
+model = Sequential()
+model.add(Embedding(vocab_size, 50, input_length=seq_length))
+model.add(LSTM(100, return_sequences=True))
+model.add(LSTM(100))
+model.add(Dense(100, activation='relu'))
+model.add(Dense(vocab_size, activation='softmax'))
+print(model.summary())
+# compile model
+model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+# fit model
+model.fit(X, y, batch_size=128, epochs=10)
+ 
+# save the model to file
+model.save('model.h5')
+# save the tokenizer
+dump(tokenizer, open('tokenizer.pkl', 'wb'))
+
+
+# In[ ]:
+
+
+from random import randint
+from pickle import load
+from keras.models import load_model
+from keras.preprocessing.sequence import pad_sequences
+ 
+# load doc into memory
+def load_doc(filename):
+	# open the file as read only
+	file = open(filename, 'r')
+	# read all text
+	text = file.read()
+	# close the file
+	file.close()
+	return text
+ 
+# generate a sequence from a language model
+def generate_seq(model, tokenizer, seq_length, seed_text, n_words):
+	result = list()
+	in_text = seed_text
+	# generate a fixed number of words
+	for _ in range(n_words):
+		# encode the text as integer
+		encoded = tokenizer.texts_to_sequences([in_text])[0]
+		# truncate sequences to a fixed length
+		encoded = pad_sequences([encoded], maxlen=seq_length, truncating='pre')
+		# predict probabilities for each word
+		yhat = model.predict_classes(encoded, verbose=0)
+		# map predicted word index to word
+		out_word = ''
+		for word, index in tokenizer.word_index.items():
+			if index == yhat:
+				out_word = word
+				break
+		# append to input
+		in_text += ' ' + out_word
+		result.append(out_word)
+	return ' '.join(result)
+ 
+# load cleaned text sequences
+in_filename = 'Data/notorious-big.txt'
+doc = load_doc(in_filename)
+lines = doc.split('\n')
+seq_length = len(lines[0].split()) - 1
+ 
+# load the model
+model = load_model('model.h5')
+ 
+# load the tokenizer
+tokenizer = load(open('tokenizer.pkl', 'rb'))
+ 
+# select a seed text
+seed_text = lines[randint(0,len(lines))]
+print(seed_text + '\n')
+ 
+# generate new text
+generated = generate_seq(model, tokenizer, seq_length, seed_text, 5)
+print(generated)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# Now using defined module to generate poetry
+
+# In[135]:
+
+
+artist_file = 'Data/notorious-big.txt'
+with open(artist_file) as f: # The with keyword automatically closes the file when you are done
+    print (f.read(1000))
+
+
+# In[136]:
+
+
+artist_file = 'Data/Lil_Wayne.txt'
+with open(artist_file) as f: # The with keyword automatically closes the file when you are done
+    print (f.read(1000))
+
+
+# In[137]:
+
+
+import numpy as np
+from matplotlib import pyplot as plt
+get_ipython().run_line_magic('matplotlib', 'inline')
+def plotWordFrequency(input):
+    f = open(artist_file,'r')
+    words = [x for y in [l.split() for l in f.readlines()] for x in y]
+    data = sorted([(w, words.count(w)) for w in set(words)], key = lambda x:x[1], reverse=True)[:40] 
+    most_words = [x[0] for x in data]
+    times_used = [int(x[1]) for x in data]
+    plt.figure(figsize=(20,10))
+    plt.bar(x=sorted(most_words), height=times_used, color = 'grey', edgecolor = 'black',  width=.5)
+    plt.xticks(rotation=45, fontsize=18)
+    plt.yticks(rotation=0, fontsize=18)
+    plt.xlabel('Most Common Words:', fontsize=18)
+    plt.ylabel('Number of Occurences:', fontsize=18)
+    plt.title('Most Commonly Used Words: %s' % (artist_file), fontsize=24)
+    plt.show()
+
+
+# In[138]:
+
+
+artist_file = 'Data/notorious-big.txt'
+plotWordFrequency(artist_file)
+
+
+# In[139]:
+
+
+artist_file = 'Data/Lil_Wayne.txt'
+plotWordFrequency(artist_file)
+
+
+# In[140]:
+
+
+import pronouncing
+import markovify
+import re
+import random
+import numpy as np
+import os
+import tensorflow as tf
+from tensorflow.python.keras.models import Sequential
+from tensorflow.python.keras.layers import LSTM
+import tensorflow.python.keras.backend as K
+from tensorflow.python.keras.layers.core import Dense
+
+
+# In[141]:
+
+
+#Recurrent Neural Network (https://keras.io/layers/recurrent/#lstm)
+
+def create_network(depth):
+	model = Sequential()
+	model.add(LSTM(4, input_shape=(2, 2), return_sequences=True))
+	for i in range(depth):
+		model.add(LSTM(8, return_sequences=True))
+	model.add(LSTM(2, return_sequences=True))
+	model.summary()
+	model.compile(optimizer='rmsprop',
+              loss='mse')
+	if artist + ".rap" in os.listdir(".") and train_mode == False:
+		model.load_weights(str(artist + ".rap"))
+		print("loading saved network: " + str(artist) + ".rap") 
+	return model
+
+
+# In[142]:
+
+
+#Markov Chain (https://github.com/jsvine/markovify)
+
+def markov(text_file):
+    ######
+	read = open(text_file, "r", encoding='utf-8').read()
+	text_model = markovify.NewlineText(read)
+	return text_model
+
+
+# In[143]:
+
+
+#Determine number of syllables in line
+
+def syllables(line):
+	count = 0
+	for word in line.split(" "):
+		vowels = 'aeiouy'
+# 		word = word.lower().strip("!@#$%^&*()_+-={}[];:,.<>/?")
+		word = word.lower().strip(".:;?!")
+		if word[0] in vowels:
+			count +=1
+		for index in range(1,len(word)):
+			if word[index] in vowels and word[index-1] not in vowels:
+				count +=1
+		if word.endswith('e'):
+			count -= 1
+		if word.endswith('le'):
+			count+=1
+		if count == 0:
+			count +=1
+	return count / maxsyllables
+
+
+# In[144]:
+
+
+#Make index of words that rhyme with your word
+
+
+def rhymeindex(lyrics):
+	if str(artist) + ".rhymes" in os.listdir(".") and train_mode == False:
+		print ("loading saved rhymes from " + str(artist) + ".rhymes")
+		return open(str(artist) + ".rhymes", "r",encoding='utf-8').read().split("\n")
+	else:
+		rhyme_master_list = []
+		print ("Building list of rhymes:")
+		for i in lyrics:
+			word = re.sub(r"\W+", '', i.split(" ")[-1]).lower()
+			rhymeslist = pronouncing.rhymes(word)
+			rhymeslistends = []      
+			for i in rhymeslist:
+				rhymeslistends.append(i[-2:])
+			try:
+				rhymescheme = max(set(rhymeslistends), key=rhymeslistends.count)
+			except Exception:
+				rhymescheme = word[-2:]
+			rhyme_master_list.append(rhymescheme)
+		rhyme_master_list = list(set(rhyme_master_list))
+		reverselist = [x[::-1] for x in rhyme_master_list]
+		reverselist = sorted(reverselist)
+		rhymelist = [x[::-1] for x in reverselist]
+		print("List of Sorted 2-Letter Rhyme Ends:")
+		print(rhymelist)
+		f = open(str(artist) + ".rhymes", "w", encoding='utf-8')
+		f.write("\n".join(rhymelist))
+		f.close()
+		return rhymelist
+
+
+# In[145]:
+
+
+#Make index of rhymes that you use
+
+def rhyme(line, rhyme_list):
+	word = re.sub(r"\W+", '', line.split(" ")[-1]).lower()
+	rhymeslist = pronouncing.rhymes(word)
+	rhymeslistends = []
+	for i in rhymeslist:
+		rhymeslistends.append(i[-2:])
+	try:
+		rhymescheme = max(set(rhymeslistends), key=rhymeslistends.count)
+	except Exception:
+		rhymescheme = word[-2:]
+	try:
+		float_rhyme = rhyme_list.index(rhymescheme)
+		float_rhyme = float_rhyme / float(len(rhyme_list))
+		return float_rhyme
+	except Exception:
+		float_rhyme = None
+		return float_rhyme
+
+
+# In[146]:
+
+
+#Separate each line of the input txt
+
+def split_lyrics_file(text_file):
+	text = open(text_file, encoding='utf-8').read()
+	text = text.split("\n")
+	while "" in text:
+		text.remove("")
+	return text
+
+
+# In[147]:
+
+
+#Generate lyrics
+
+def generate_lyrics(text_model, text_file):
+	bars = []
+	last_words = []
+	lyriclength = len(open(text_file,encoding='utf-8').read().split("\n"))
+	count = 0
+	markov_model = markov(text_file)
+	
+	while len(bars) < lyriclength / 9 and count < lyriclength * 2:
+		bar = markov_model.make_sentence(max_overlap_ratio = .49, tries=100)
+		if type(bar) != type(None) and syllables(bar) < 1:
+			def get_last_word(bar):
+				last_word = bar.split(" ")[-1]
+				if last_word[-1] in "!.?,":
+					last_word = last_word[:-1]
+				return last_word
+			last_word = get_last_word(bar)
+			if bar not in bars and last_words.count(last_word) < 3:
+				bars.append(bar)
+				last_words.append(last_word)
+				count += 1
+	return bars
+
+
+# In[148]:
+
+
+#Buliding Dataset
+def build_dataset(lines, rhyme_list):
+	dataset = []
+	line_list = []
+	for line in lines:
+		line_list = [line, syllables(line), rhyme(line, rhyme_list)]
+		dataset.append(line_list)
+	x_data = []
+	y_data = []
+	for i in range(len(dataset) - 3):
+		line1 = dataset[i    ][1:]
+		line2 = dataset[i + 1][1:]
+		line3 = dataset[i + 2][1:]
+		line4 = dataset[i + 3][1:]
+		x = [line1[0], line1[1], line2[0], line2[1]]
+		x = np.array(x)
+		x = x.reshape(2,2)
+		x_data.append(x)
+		y = [line3[0], line3[1], line4[0], line4[1]]
+		y = np.array(y)
+		y = y.reshape(2,2)
+		y_data.append(y)
+	x_data = np.array(x_data)
+	y_data = np.array(y_data)
+	return x_data, y_data
+
+
+# In[149]:
+
+
+#Compose verse
+
+def compose_rap(lines, rhyme_list, lyrics_file, model):
+	rap_vectors = []
+	human_lyrics = split_lyrics_file(lyrics_file)
+	initial_index = random.choice(range(len(human_lyrics) - 1))
+	initial_lines = human_lyrics[initial_index:initial_index + 2]
+	starting_input = []
+	for line in initial_lines:
+		starting_input.append([syllables(line), rhyme(line, rhyme_list)])
+	starting_vectors = model.predict(np.array([starting_input]).flatten().reshape(1, 2, 2))
+	rap_vectors.append(starting_vectors)
+	for i in range(100):
+		rap_vectors.append(model.predict(np.array([rap_vectors[-1]]).flatten().reshape(1, 2, 2)))
+	return rap_vectors
+
+
+# In[150]:
+
+
+#Compose verse (part 2)
+
+def vectors_into_song(vectors, generated_lyrics, rhyme_list):
+	print ("\n\n")	
+	print ("Writing verse:")
+	print ("\n\n")
+	def last_word_compare(rap, line2):
+		penalty = 0 
+		for line1 in rap:
+			word1 = line1.split(" ")[-1]
+			word2 = line2.split(" ")[-1]
+			while word1[-1] in "?!,. ":
+				word1 = word1[:-1]
+			while word2[-1] in "?!,. ":
+				word2 = word2[:-1]
+			if word1 == word2:
+				penalty += 0.2
+		return penalty
+	def calculate_score(vector_half, syllables, rhyme, penalty):
+		desired_syllables = vector_half[0]
+		desired_rhyme = vector_half[1]
+		desired_syllables = desired_syllables * maxsyllables
+		desired_rhyme = desired_rhyme * len(rhyme_list)
+		score = 1.0 - abs(float(desired_syllables) - float(syllables)) + abs(float(desired_rhyme) - float(rhyme)) - penalty
+		return score
+	dataset = []
+	for line in generated_lyrics:
+		line_list = [line, syllables(line), rhyme(line, rhyme_list)]
+		dataset.append(line_list)
+	rap = []
+	vector_halves = []
+	for vector in vectors:
+		vector_halves.append(list(vector[0][0])) 
+		vector_halves.append(list(vector[0][1]))
+	for vector in vector_halves:
+		scorelist = []
+		for item in dataset:
+			line = item[0]
+			if len(rap) != 0:
+				penalty = last_word_compare(rap, line)
+			else:
+				penalty = 0
+			total_score = calculate_score(vector, item[1], item[2], penalty)
+			score_entry = [line, total_score]
+			scorelist.append(score_entry)
+		fixed_score_list = [0]
+		for score in scorelist:
+			fixed_score_list.append(float(score[1]))
+		max_score = max(fixed_score_list)
+		for item in scorelist:
+			if item[1] == max_score:
+				rap.append(item[0])
+				print (str(item[0]))
+				for i in dataset:
+					if item[0] == i[0]:
+						dataset.remove(i)
+						break
+				break     
+	return rap
+
+
+# In[151]:
+
+
+#Traning function
+
+def train(x_data, y_data, model):
+	model.fit(np.array(x_data), np.array(y_data),
+			  batch_size=2,
+			  epochs=5,
+			  verbose=1)
+	model.save_weights(artist + ".rap")
+
+
+# In[152]:
+
+
+#Train and run the model
+
+def main(depth, train_mode):
+	model = create_network(depth)
+	text_model = markov(text_file)
+	if train_mode == True:
+		bars = split_lyrics_file(text_file)
+	if train_mode == False:
+		bars = generate_lyrics(text_model, text_file)
+	rhyme_list = rhymeindex(bars)
+	if train_mode == True:
+		x_data, y_data = build_dataset(bars, rhyme_list)
+		train(x_data, y_data, model)
+	if train_mode == False:
+		vectors = compose_rap(bars, rhyme_list, text_file, model)
+		rap = vectors_into_song(vectors, bars, rhyme_list)
+		f = open(rap_file, "w", encoding='utf-8')
+		for bar in rap:
+			f.write(bar)
+			f.write("\n")
+
+
+# Some important parameters to keep in mind are as follows: (1) maxsyllables: max # of syllables per line; (2) max_overlap_ratio: how different the output should be from the original input; (3) tries: how many times to try to build a line that satisifies some parameter; (4) epochs: how many times for the NN to pass over the data.
+# 
+# To reduce computational complexity, maximize (1) and (2) and minimize (3) and (4).
+# Computational time will increase dramatically when (1) is less than 8 and when (2) is less than 0.5
+
+# In[153]:
+
+
+depth = 4 
+maxsyllables = 8
+artist = "artist"
+rap_file = "temporary_poem.txt"
+
+
+# In[ ]:
+
+
+#Write new lyrics in the style of Notorious B.I.G..
+# import tensorflow as tf
+# from tensorflow.python.framework import ops
+# ops.reset_default_graph()
+maxsyllables = 8
+text_file = "Data/notorious-big.txt"
+train_mode = True        
+main(depth, train_mode)
+train_mode = False
+main(depth, train_mode)
+
+
+# In[ ]:
+
+
+
+
+
+# In[52]:
+
+
+#Write new lyrics in the combined style of Notorious B.I.G. and Lil Wayne.
+
+filenames = ['Data/Lil_Wayne.txt', 'Data/notorious-big.txt']
+with open('combined.txt', 'w') as outfile:
+    for fname in filenames:
+        with open(fname) as infile:
+            for line in infile:
+                outfile.write(line)
+
+
+# In[53]:
+
+
+#artist_file = '../input/combined.txt'
+artist_file = 'combined.txt'
+plotWordFrequency(artist_file)
+
+
+# In[ ]:
+
+
+maxsyllables = 8
+text_file = "combined.txt"
+train_mode = True        
+main(depth, train_mode)
+train_mode = False
+main(depth, train_mode)
+
+
+# In[ ]:
+
+
+
+
